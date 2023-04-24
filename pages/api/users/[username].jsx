1cac9ac46bdf0487c0e35c0274501d44e3f148e4
Ralph Lavelle <ralphlavelle@gmail.com>
2023-04-24 08:15:54 +0300

    Removed email part

--- pages/api/users/[username].jsx
+++ pages/api/users/[username].jsx
@@ -1,5 +1,4 @@
 import { MongoDB } from '../../../components/MongoDB';
-var postmark = require("postmark");
 
 export default async function handler(req, res) {
 	const { username, password, code, action } = req.query;
@@ -28,23 +27,10 @@ export default async function handler(req, res) {
 					code
 				};
 
-				// send the user an email
 				let query = { username };
 				const { results, client } = await mongo.get("users", query, true);
-				const emailClient = new postmark.ServerClient(process.env.NEXT_PUBLIC_POSTMARK_API_TOKEN);
-
-				console.log(`email: ${results.email}`);
 
 				if(results) {
-					emailClient.sendEmail({
-						"From": process.env.NEXT_PUBLIC_POSTMARK_FROM_ADDRESS,
-						"To": results.email,
-						"Subject": "Bot Controller verification code",
-						"HtmlBody": `Hello <strong>${results.username}</strong>, the Bot Controller verification code is ${code}`,
-						"TextBody": `Hello ${results.username}, the Bot Controller verification code is ${code}`,
-						"MessageStream": "outbound"
-					});
-
 					// save this to the db for this user
 					await mongo.save(user);
 					res.status(200).json(true);
@@ -69,7 +55,5 @@ export default async function handler(req, res) {
 			}
 			client.close();
 		}
-	} else {
-		// Process a POST request	
 	}
 }
