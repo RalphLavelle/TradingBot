7a26678317d7a523a79b08aafd27ede94351c919
Ralph Lavelle <ralphlavelle@gmail.com>
2023-05-01 16:25:54 +0300

    Server error

--- pages/api/users/[username].jsx
+++ pages/api/users/[username].jsx
@@ -59,7 +59,7 @@ export default async function handler(req, res) {
 			// 	res.status(404).json(false);
 			// }
 			res.status(200).json(true);
-			client.close();
+			// client.close();
 		}
 	}
 }
